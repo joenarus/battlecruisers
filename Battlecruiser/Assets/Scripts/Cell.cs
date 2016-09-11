fe161a172d63bb573535dc3acd06117d7a1359ff
joenarus <joe.narus2@gmail.com>
2016-09-10 21:36:58 -0600

    Grid has been created.

---
+++ Battlecruiser/Assets/Scripts/Cell.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * A data object storing various information about the cells within the grid. 
+ * 
+ */
+public class Cell : MonoBehaviour {
+
+	public bool occupied; 
+	public bool visible;
+
+	//Various coordinate values of the cell numbered 1 - 6
+	private int x; 
+	private int y;
+	private int z;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Initialize(int _x, int _y, int _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+
+        occupied = false;
+        visible = false;
+    }
+
+	public int get_x() {
+		return x;
+	}
+
+	public int get_y() {
+		return y;
+	}
+
+	public int get_z() {
+		return z;
+	}
+
+}
---
+++ Battlecruiser/Assets/Scripts/Grid.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grid : MonoBehaviour {
+
+	public List<Cell> cells;
+    public grid_overlay highlights;
+
+    public GameObject cell;
+
+	//Stores how big the grid is
+	public int x_columns = 1;
+	public int y_columns = 1;
+	public int z_columns = 1;
+
+	// Use this for initialization
+	void Start () {
+        highlights.gridSizeX = x_columns;
+        highlights.gridSizeY = y_columns;
+        highlights.gridSizeZ = z_columns;
+
+        for(int i = 0; i < x_columns; i ++)
+        {
+            for (int j = 0; j < y_columns; j++)
+            {
+                for (int k = 0; k < z_columns; k++)
+                {
+                    float x = i + .5f;
+                    float y = j + .5f;
+                    float z = k + .5f;
+                   GameObject newCell = Object.Instantiate(cell, new Vector3(x,y,z), Quaternion.identity) as GameObject;
+                   Cell temp = newCell.GetComponent<Cell>();
+                   temp.Initialize(i, j, k);
+                }
+            }
+        }
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void set_x_columns(int _x)
+    {
+        x_columns = _x;
+    }
+    public int get_x_colums()
+    {
+        return x_columns;
+    }
+    public void set_y_columns(int _y)
+    {
+        y_columns = _y;
+    }
+    public int get_y_colums()
+    {
+        return y_columns;
+    }
+    public void set_z_columns(int _z)
+    {
+        z_columns = _z;
+    }
+    public int get_z_colums()
+    {
+        return z_columns;
+    }
+
+}
