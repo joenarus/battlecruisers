619adc3354a53aa914fd721b2e7e048728980ceb
joenarus <joe.narus2@gmail.com>
2016-09-13 01:30:36 -0600

    Rotation

--- Battlecruiser/Assets/Scripts/Player.cs
+++ Battlecruiser/Assets/Scripts/Player.cs
@@ -4,32 +4,36 @@ using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
 
+    private bool _moved;
+    private bool _attacked;
+    private List<Ship> _ships;
+
     public bool moved
     {
-        get { return moved; }
-        set { moved = value; }
+        get { return _moved; }
+        set { _moved = value; }
     }
 
     public bool attacked
     {
-        get { return attacked; }
-        set { attacked = value; }
+        get { return _attacked; }
+        set { _attacked = value; }
     }
 
     private List<Ship> ships
     {
-        get { return ships; }
-        set { ships = value; }
+        get { return _ships; }
+        set { _ships = value; }
     }
 
     public void addShip(Ship s)
     {
-        ships.Add(s);
+        _ships.Add(s);
     }
 
 	// Use this for initialization
 	void Start () {
-        ships = new List<Ship>();
+        _ships = new List<Ship>();
 	}
 	
 	// Update is called once per frame
--- Battlecruiser/Assets/Scripts/game_controller.cs
+++ Battlecruiser/Assets/Scripts/game_controller.cs
@@ -19,6 +19,8 @@ public class game_controller : MonoBehaviour {
 	void Start () {
         status.text = "Turn: Player 1";
         player_turn = 1;
+        player1 = new Player();
+        player2 = new Player();
     }
 	
 	// Update is called once per frame
@@ -32,11 +34,22 @@ public class game_controller : MonoBehaviour {
         {
             status.text = "Turn: Player 2";
             player_turn = 2;
+            player1.moved = true;
+            player1.attacked = true;
+
+            player2.moved = false;
+            player2.attacked = false;
+
         }
         else
         {
             status.text = "Turn: Player 1";
             player_turn = 1;
+            player1.moved = false;
+            player1.attacked = false;
+
+            player2.moved = true;
+            player2.attacked = true;
         }
     }
 
@@ -44,8 +57,51 @@ public class game_controller : MonoBehaviour {
     {
         
         Ship moving_ship = battlefield.get_selected_ship();
+        Vector3 forward = moving_ship.transform.forward;
+        Debug.Log(forward);
         if (moving_ship != null)
-            moving_ship.transform.Translate(1,0,0);
+        {
+            moving_ship.transform.Translate(1, 0, 0);
+
+            if (player_turn == 1)
+            {
+                player1.moved = true;
+            }
+            else
+                player2.moved = true;
+            {
+
+            }
+        }
+    }
+
+    // Right: x = 1
+    // Left: x = 2
+    public void turnShip(int x)
+    {
+        
+        Ship moving_ship = battlefield.get_selected_ship();
+        if (moving_ship != null)
+        {
+            if (x == 1)
+            {
+                moving_ship.transform.Rotate(0, 90, 0);
+            }
+            if (x == 2)
+            {
+                moving_ship.transform.Rotate(0, -90, 0);
+            }
+
+            if (player_turn == 1)
+            {
+                player1.moved = true;
+            }
+            if (player_turn == 2)
+                player2.moved = true;
+            {
+
+            }
+        }
     }
 
 }
